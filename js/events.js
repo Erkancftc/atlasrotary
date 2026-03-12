ecf5658fda1c41eace4c7967145d5ec8162be99a
kaan doğan <wowfalcon535@gmail.com>
2026-03-12 17:49:09 +0300

    321

--- js/events.js
+++ js/events.js
@@ -1,27 +1,4 @@
-// ;<div class="col-lg-4">
-//   <div class="blog-content">
-//     <div class="blog-item blog-item1">
-//       <div class="blog-img">
-//         <a href="">
-//           <img src="images/duyu-butunleme/04.jpg" alt="duyu bütünleme" />
-//         </a>
-//         <span class="blog__tag blog__tag1">
-//           <span class="date__num-text">3</span>
-//           <span class="date__mon-text">Aralık</span>
-//         </span>
-//       </div>
-//       <div class="blog-inner-content">
-//         <h3 class="blog__title">
-//           <a href="events-detail.html?slug=duyu-butunleme-merkezi-acilisi">Duyu Bütünleme</a>
-//         </h3>
-//         <ul class="blog__list">
-//           <li class="blog__dot-active">03 Aralık 2025</li>
-//           <li>Ataşehir</li>
-//         </ul>
-//       </div>
-//     </div>
-//   </div>
-// </div>
+  
 ;(() => {
   "use strict"
 
@@ -45,11 +22,12 @@ async function fetchEvents() {
     return [];
   }
 }
-
 /**
  * Create a card element for a single event and append it to the page.
  * @param {Object} ev event data
  */
+
+
 function createCard(ev) {
   const card = document.createElement("div");
   card.className = "col-lg-4";
@@ -79,6 +57,8 @@ function createCard(ev) {
   `;
   blogContent.appendChild(card);
 }
+
+
 
 /**
  * Sort an array of events by date (ascending).
